58be97e3eb6dc7d1a430c7526f1aca543cf2678f
pragdave <dave@pragdave.me>
2016-09-08 10:58:39 -0500

    embed docs from Pollution into the README

---
+++ lib/mix/tasks/update_readme.ex
@@ -0,0 +1,34 @@
+defmodule Mix.Tasks.UpdateReadme do
+  use Mix.Task
+
+  @shortdoc "update README.md with the documentation from Pollution.VG"
+  def run(_) do
+    opts = %{
+      source_url_pattern: "",
+      source_root: "",
+    }
+
+    [mod] = ExDoc.Retriever.docs_from_modules([Pollution.VG], opts)
+
+    result =
+      Enum.reduce(mod.docs, ["<!-- pollution -->"], &fn_docs/2)
+      |> Enum.reverse
+      |> Enum.join("\n")
+
+    readme =
+        File.read!("README.md")
+        |> String.replace("<!-- pollution -->", result)
+
+
+    File.write!("README.md", readme)
+  end
+
+  def fn_docs(f = %ExDoc.FunctionNode{doc: nil}, result) do
+    [ "\n* #{f.signature}" | result ]
+  end
+
+  def fn_docs(f = %ExDoc.FunctionNode{}, result) do
+    indented_doc = String.split(f.doc, "\n") |> Enum.map(&"  #{&1}") |> Enum.join("\n")
+    [ indented_doc, "\n* #{f.signature}" | result ]
+  end
+end

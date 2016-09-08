8a0291a52487c9a44666974ddf4d69469b744373
pragdave <dave@pragdave.me>
2016-09-08 15:12:24 -0500

    embed docs from Pollution into the README

--- lib/mix/tasks/update_readme.ex
+++ lib/mix/tasks/update_readme.ex
@@ -39,6 +39,6 @@ defmodule Mix.Tasks.UpdateReadme do
   end
 
   def signature(sig) do
-    "\n* \#\# `#{String.replace(sig, "\\\\", "\\\\\\\\")}`\n"
+    "\n* ### `#{String.replace(sig, "\\\\", "\\\\\\\\")}`\n"
   end
 end
